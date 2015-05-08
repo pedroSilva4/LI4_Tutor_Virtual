dabdbed1b934773a98cde1416a0b4ffded53079d
pedroSilva4 <a64345@alunos.uminho.pt>
2015-05-08 11:40:31 +0100

    first commit

---
+++ NomesDaHistoriaApplication/NomesDaHistoriaApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NomesDaHistoriaApplication.Startup))]
+namespace NomesDaHistoriaApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
