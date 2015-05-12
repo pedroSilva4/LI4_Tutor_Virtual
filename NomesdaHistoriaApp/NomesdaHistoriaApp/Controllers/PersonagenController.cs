9e0266d8f2ab329d43bf9a6e38dbab2a9d0836f0
xavierlei <a55838@alunos.uminho.pt>
2015-05-12 14:59:30 +0100

    xxxxxxxxx

---
+++ NomesdaHistoriaApp/NomesdaHistoriaApp/Controllers/PersonagenController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NomesdaHistoriaApp.Models;
+
+namespace NomesdaHistoriaApp.Controllers
+{
+    public class PersonagenController : Controller
+    {
+        // GET: Avatar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MostraTutor()
+        {
+            UtilizadorDBContext userDB = new UtilizadorDBContext();
+            Utilizador u = new Utilizador();
+            u = userDB.Utilizadores.Find("pedrinho");
+           // string avatar = u.avatar;
+            PerosnagensDBContext personagem = new PerosnagensDBContext();
+            Personagen p = new Personagen();
+            p = personagem.Personagens.Find(u.avatar.ToString());
+            return View(p);   
+        }
+    }
+}
